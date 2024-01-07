d837472e6f97099b0b14f8c65b74d0188711c32e
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2024-01-07 13:50:12 +0200

    change007

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const {
-  getAllContacts,
-  getById,
-  removeById,
-  newContact,
-  updById,
-  updateStatusContact,
-} = require("../../controllers/index");
+  register,
+  login,
+  getCurrentUser,
+  logout,
+} = require("../../controllers/users/index");
 
 const router = express.Router();
 
 const isValidId = require("../../midddlewars/isValidId");
 const authenticate = require("../../midddlewars/authenticate");
 
-router.get("/", authenticate, isValidId, getAllContacts);
-router.get("/:contactId", authenticate, getById);
-router.post("/", authenticate, isValidId, newContact);
-router.delete("/:contactId", authenticate, removeById);
-router.put("/:contactId", authenticate, updById);
-router.patch("/:contactId/favorite", authenticate, updateStatusContact);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/current", authenticate, isValidId, getCurrentUser);
+router.post("/logout", authenticate, isValidId, logout);
 
 module.exports = router;
