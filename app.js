24e600ad971f16accb2513c9047b55abdd6ff273
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2024-01-14 17:46:40 +0200

    chane010

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
+import express from "express";
+import logger from "morgan";
+import cors from "cors";
+import "dotenv/config";
 
-require("dotenv").config();
-
-const contactsRouter = require("./routes/api/contacts");
-const userRouter = require("./routes/api/users");
+import authRouter from "./routes/api/auth-router.js";
+import moviesRouter from "./routes/api/movies-router.js";
 
 const app = express();
 
@@ -15,15 +14,18 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRouter);
-app.use("/api/contacts", contactsRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/movies", moviesRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({
+    message,
+  });
 });
 
-module.exports = app;
+export default app;
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-const app = require("./app");
-const mongoose = require("mongoose");
-const { DB_HOST } = process.env;
+import mongoose from "mongoose";
+
+import app from "./app.js";
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
 mongoose
   .connect(DB_HOST)
-  .then(() =>
-    app.listen(3000, () => {
-      console.log("Database connection successful");
-    })
-  )
-  .catch((err) => {
-    console.log("err.message");
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
     process.exit(1);
   });
