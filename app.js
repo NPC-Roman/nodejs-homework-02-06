a9f95d06449d758cdcbf2b55ff8b827068b6bd01
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2024-01-17 18:06:52 +0200

    change14

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import "dotenv/config";
-
+/*
 import authRouter from "./routes/api/auth-router.js";
 import moviesRouter from "./routes/api/movies-router.js";
-/*
+*/
 import authRouter from "./project-backend/routes/api/auth-router.js";
 import moviesRouter from "./project-backend/routes/api/movies-router.js";
-*/
 
 const app = express();
 
